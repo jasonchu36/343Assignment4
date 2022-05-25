231832192a27f7d62c5503d8018fd5a68e343668
TheKingSparta <zachclow2013@gmail.com>
2022-05-24 18:11:57 -0700

    Create comedy.h

---
+++ comedy.h
@@ -0,0 +1,29 @@
+/**         comedy.h
+ *
+ * Created by Zachary Clow
+ *
+ * Defines a Comedy class that inherits from Movie.
+ *
+ */
+
+#ifndef ASSIGNMENT4_COMEDY_H
+#define ASSIGNMENT4_COMEDY_H
+
+#include "movie.h"
+#include <string>
+
+class Comedy: public Movie {
+public:
+   //Default constructor. Defined in Movie.h
+   Comedy();
+
+   ~Comedy();
+
+   //Constructor to set default values
+   Comedy(int stock, std::string title, std::string director, int releaseYear);
+
+   //Returns a string representation of the Comedy
+   std::string toString();
+};
+
+#endif //ASSIGNMENT4_COMEDY_H
